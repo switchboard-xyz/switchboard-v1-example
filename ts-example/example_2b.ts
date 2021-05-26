4a4e8ca88dc45edadc670c62f9afe14cabe4ae04
Mitchell Gildenberg <mgild@umich.edu>
2021-05-26 06:33:38 -0400

    Add example 2b file

---
+++ ts-example/example_2b.ts
@@ -0,0 +1,109 @@
+import {
+  Account,
+  Cluster,
+  Context,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+  SignatureResult,
+} from '@solana/web3.js';
+import {
+  AggregatorState,
+  FulfillmentManagerAuth,
+  FulfillmentManagerState,
+  OracleJob,
+  SWITCHBOARD_DEVNET_PID,
+  SwitchboardAccountType,
+  SwitchboardInstruction,
+  addFeedJob,
+  createDataFeed,
+  createFulfillmentManager,
+  createFulfillmentManagerAuth,
+  createOwnedStateAccount,
+  initFulfillmentManagerAccount,
+  publishSwitchboardAccount,
+  removeFeedJob,
+  setDataFeedConfigs,
+  setFulfillmentManagerConfigs,
+  updateFeed,
+} from '@switchboard-xyz/switchboard-api';
+import { EventEmitter } from "events";
+import { waitFor } from "wait-for-event";
+import yargs = require('yargs/yargs');
+const resolve = require('resolve-dir');
+const bs58 = require('bs58');
+const fs = require('fs');
+
+let argv = yargs(process.argv).options({
+  'updateAuthPubkey': {
+    type: 'string',
+    describe: 'The public key of the auth account permitting data feed updates.',
+    demand: true,
+  },
+  'dataFeedPubkey': {
+    type: 'string',
+    describe: 'The public key of the data feed being updated.',
+    demand: true,
+  },
+  'payerFile': {
+    type: 'string',
+    describe: 'Keypair file to pay for transactions.',
+    demand: true,
+  },
+}).argv;
+
+function toCluster(cluster: string): Cluster {
+  switch (cluster) {
+    case "devnet":
+    case "testnet":
+    case "mainnet-beta": {
+      return cluster;
+    }
+  }
+  throw new Error("Invalid cluster provided.");
+}
+
+function waitForever(): Promise<void> {
+  return waitFor("", new EventEmitter());
+}
+
+async function main() {
+  let cluster = 'devnet';
+  let connection = new Connection(clusterApiUrl(toCluster(cluster), true), 'finalized');
+  let payerKeypair = JSON.parse(fs.readFileSync(resolve(argv.payerFile), 'utf-8'));
+  let payerAccount = new Account(payerKeypair);
+  let dataFeedPubkey = new PublicKey(argv.dataFeedPubkey);
+  let updateAuthPubkey = new PublicKey(argv.updateAuthPubkey);
+  let signature = await updateFeed(
+    connection,
+    payerAccount, 
+    dataFeedPubkey,
+    updateAuthPubkey
+  );
+  console.log("Feed update transaction submitted. Awaiting finalization...");
+  let callback = async function (signatureResult: SignatureResult, ctx: Context) {
+    let feedAccountInfo = await connection.getAccountInfo(dataFeedPubkey);
+    let state: AggregatorState = AggregatorState.decodeDelimited(feedAccountInfo.data.slice(1));
+    console.log(`(${dataFeedPubkey.toBase58()}) state.\n`,
+                JSON.stringify(state.toJSON(), null, 2));
+    process.exit(0);
+  };
+  connection.onSignature(signature, callback, 'finalized');
+  await waitForever();
+}
+
+main().then(
+  () => process.exit(),
+  err => {
+    console.error("Failed to complete action.");
+    console.error(err);
+    process.exit(-1);
+  },
+);
