4550d798368ba4c33aa2a52146a8dd3eda79839e
Mitchell Gildenberg <mgild@umich.edu>
2021-05-25 23:40:14 -0400

    Example 2 setup

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ In this example, we will post an example Solana program that will parse and
 print a provided data feed.
 
 ```
-cd $(git rev-parse --show-toplevel)
-cd example-program
+cd "$(git rev-parse --show-toplevel)/example-program"
 cargo build-bpf --manifest-path=Cargo.toml --bpf-out-dir=$PWD
 PROGRAM_PUBKEY=$(solana program deploy switchboard_example.so | tee /dev/tty | grep "Program Id:" | awk '{print $NF}')
 cd ../ts-example
@@ -15,5 +14,34 @@ solana-keygen new --outfile example-keypair.json
 solana airdrop 5 example-keypair.json
 # Find Data Feed Pubkeys at https://switchboard.xyz/#/explorer
 FEED_PUBKEY="<YOUR FEED PUBKEY HERE>"
-ts-node main.ts --payerFile=example-keypair.json --programPubkey=${PROGRAM_PUBKEY?} --dataFeedPubkey=${FEED_PUBKEY?}
+ts-node example_1.ts --payerFile=example-keypair.json --programPubkey=${PROGRAM_PUBKEY?} --dataFeedPubkey=${FEED_PUBKEY?}
+```
+
+# Example 2: Creating your own Data Feed
+
+In this example, we will create our own data feed and spin up our own node to
+fulfill aggregator jobs.
+
+In part `a` of this example, we will:
+1. Create a data feed.
+1. Add an example job to the feed.
+1. Create a fulfillment manager and link it to the data feed.
+1. Run a Switchboard node on the new fulfillment manager.
+
+In part `b` we will:
+1. Call `update` on a Switchboard Feed.
+1. Watch as the aggregator populates with results!
+
+Part a:
+```
+cd "$(git rev-parse --show-toplevel)/ts-example"
+solana airdrop 5 example-keypair.json
+ts-node example_2a.ts --payerFile=example-keypair.json
+export FULFILLMENT_MANAGER_KEY=<FULFILLMENT MANAGER KEY HERE>
+export AUTH_KEY=<AUTH KEY HERE>
+```
+
+Part b:
+```
+ts-node example_2b.ts --payerFile=example-keypair.json
 ```
---
+++ ts-example/example_2a.ts
@@ -0,0 +1,115 @@
+import {
+  Account,
+  Cluster,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import {
+  AggregatorState,
+  FulfillmentManagerAuth,
+  FulfillmentManagerState,
+  OracleJob,
+  SWITCHBOARD_DEVNET_PID,
+  SwitchboardAccountType,
+  SwitchboardInstruction,
+  addFeedJob,
+  createDataFeed,
+  createFulfillmentManager,
+  createFulfillmentManagerAuth,
+  createOwnedStateAccount,
+  initFulfillmentManagerAccount,
+  publishSwitchboardAccount,
+  removeFeedJob,
+  setDataFeedConfigs,
+  setFulfillmentManagerConfigs,
+  updateFeed,
+} from '@switchboard-xyz/switchboard-api';
+import yargs = require('yargs/yargs');
+const resolve = require('resolve-dir');
+const bs58 = require('bs58');
+const fs = require('fs');
+
+let argv = yargs(process.argv).options({
+  'payerFile': {
+    type: 'string',
+    describe: "Keypair file to pay for transactions.",
+    demand: true,
+  },
+}).argv;
+
+function toCluster(cluster: string): Cluster {
+  switch (cluster) {
+    case "devnet":
+    case "testnet":
+    case "mainnet-beta": {
+      return cluster;
+    }
+  }
+  throw new Error("Invalid cluster provided.");
+}
+
+async function main() {
+  let cluster = 'devnet';
+  let connection = new Connection(clusterApiUrl(toCluster(cluster), true), 'processed');
+  let payerKeypair = JSON.parse(fs.readFileSync(resolve(argv.payerFile), 'utf-8'));
+  let payerAccount = new Account(payerKeypair);
+  console.log("Creating aggregator...");
+  let dataFeedAccount = await createDataFeed(connection, payerAccount, SWITCHBOARD_DEVNET_PID);
+  console.log("Adding job to aggregator...");
+  await addFeedJob(connection, payerAccount, dataFeedAccount, [
+    OracleJob.Task.create({
+      httpTask: OracleJob.HttpTask.create({
+        url: `https://www.binance.com/api/v3/ticker/price?symbol=${pair}`
+      }),
+    }),
+    OracleJob.Task.create({
+      jsonParseTask: OracleJob.JsonParseTask.create({ path: "$.price" }),
+    }),
+  ]);
+
+  console.log(`FEED_PUBKEY=${dataFeedAccount.publicKey}`);
+  console.log("Creating fulfillment manager...");
+  let fulfillmentManagerAccount = await createFulfillmentManager(connection, payerAccount, SWITCHBOARD_DEVNET_PID);
+  await setFulfillmentManagerConfigs(connection, payerAccount, fulfillmentManagerAccount, {
+      "heartbeatAuthRequired": true,
+      "usageAuthRequired": true,
+      "lock": false
+    });
+  console.log(`FULFILLMENT_MANAGER_KEY=${fulfillmentManagerAccount.publicKey}`);
+  console.log("Configuring aggregator...");
+  await setDataFeedConfigs(connection, payerAccount, dataFeedAccount, {
+    "minConfirmations": 1,
+    "minUpdateDelaySeconds": 5,
+    "fulfillmentManagerPubkey": fulfillmentManagerAccount.publicKey.toBuffer(),
+    "lock": false
+  });
+  console.log(`Creating authorization account to permit account `
+              + `${payerAccount.publicKey} to join fulfillment manager ` +
+                `${fulfillmentManagerAccount.publicKey}`);
+  let authAccount = await createFulfillmentManagerAuth(
+    connection,
+    payerAccount,
+    fulfillmentManagerAccount,
+    payerAccount.publicKey, {
+        authorizeHeartbeat: true,
+        authorizeUsage: false
+    });
+  console.log(`AUTH_KEY=${authAccount.publicKey}`);
+}
+
+main().then(
+  () => process.exit(),
+  err => {
+    console.error("Failed to complete action.");
+    console.error(err);
+    process.exit(-1);
+  },
+);
