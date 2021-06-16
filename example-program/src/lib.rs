7489f2902d9b5ae4a00612edfd8b2e90d8b42b45
Mitchell Gildenberg <mgild@umich.edu>
2021-06-16 13:44:25 -0700

    borrow account bufer once

--- example-program/src/lib.rs
+++ example-program/src/lib.rs
@@ -14,24 +14,20 @@ use switchboard_program::{
 entrypoint!(process_instruction);
 
 fn process_instruction<'a>(
-    program_id: &'a Pubkey,
+    _program_id: &'a Pubkey,
     accounts: &'a [AccountInfo],
-    instruction_data: &'a [u8],
+    _instruction_data: &'a [u8],
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
     let switchboard_feed_account = next_account_info(accounts_iter)?;
     let mut out = 0.0;
-    if switchboard_feed_account.try_borrow_data()?[0]
-        == SwitchboardAccountType::TYPE_AGGREGATOR as u8
-    {
+    let account_buf = switchboard_feed_account.try_borrow_data()?;
+    if account_buf[0] == SwitchboardAccountType::TYPE_AGGREGATOR as u8 {
         let aggregator: AggregatorState = get_aggregator(switchboard_feed_account)?;
         let round_result: RoundResult = get_aggregator_result(&aggregator)?;
         out = round_result.result.unwrap_or(0.0);
-    } else if switchboard_feed_account.try_borrow_data()?[0]
-        == SwitchboardAccountType::TYPE_AGGREGATOR_RESULT_PARSE_OPTIMIZED as u8
-    {
-        let buf = switchboard_feed_account.try_borrow_data()?;
-        let feed_data = FastRoundResultAccountData::deserialize(&buf).unwrap();
+    } else if account_buf[0] == SwitchboardAccountType::TYPE_AGGREGATOR_RESULT_PARSE_OPTIMIZED as u8 {
+        let feed_data = FastRoundResultAccountData::deserialize(&account_buf).unwrap();
         out = feed_data.result.result;
     } else {
         return Err(ProgramError::InvalidAccountData);
