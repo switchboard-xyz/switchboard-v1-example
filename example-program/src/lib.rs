3373117c829cf80eb0f0640236878eb3d4664610
Mitchell Gildenberg <mgild@umich.edu>
2021-06-23 12:33:07 -0700

    Add better errors for aggregator call fails

--- example-program/src/lib.rs
+++ example-program/src/lib.rs
@@ -23,8 +23,16 @@ fn process_instruction<'a>(
     let mut out = 0.0;
     let account_buf = switchboard_feed_account.try_borrow_data()?;
     if account_buf[0] == SwitchboardAccountType::TYPE_AGGREGATOR as u8 {
-        let aggregator: AggregatorState = get_aggregator(switchboard_feed_account)?;
-        let round_result: RoundResult = get_aggregator_result(&aggregator)?;
+        let aggregator: AggregatorState = get_aggregator(switchboard_feed_account)
+            .map_err(|e| {
+                msg!("Aggregator parse failed. Please double check the provided address.");
+                return e;
+            })?;
+        let round_result: RoundResult = get_aggregator_result(&aggregator)
+            .map_err(|e| {
+                msg!("Failed to parse an aggregator round. Has update been called on the aggregator?");
+                return e;
+            })?;
         out = round_result.result.unwrap_or(0.0);
     } else if account_buf[0] == SwitchboardAccountType::TYPE_AGGREGATOR_RESULT_PARSE_OPTIMIZED as u8 {
         let feed_data = FastRoundResultAccountData::deserialize(&account_buf).unwrap();
